c0d844d3e1d018096b90817498c67f3e9c6c6260
afrag61 <a.frag166@gmail.com>
2024-11-19 04:05:55 +0200

    exercise (Fix Errors) Passed success

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 
 // don't change the Component name "App"
 export default function App() {
-    const clickHandler = () => {
+  let [click, setClick] = React.useState()
+    function clickHandler(value){
+      setClick(value)
         console.log('Clicked!');
     };
     
     return (
-        <Div>
+        <div>
           <h2>You're logged in!</h2>
           <p>Welcome to your user profile!</p>
-          <button onclick={clickHandler}>Click me!</button>
-        </Div>
+          <button onClick={() => clickHandler(true)}>Click me!</button>
+        </div>
     );
 }
