c611ce870a47b12b25b7819cfd3908d1811dbd66
afrag61 <a.frag166@gmail.com>
2024-11-19 20:23:07 +0200

    Ready To Code

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,5 @@
-import React from "react";
-
-function App() {
-  const input = React.useRef()
-
-  function handleClick(){
-    input.current.click()
-  }
-
+export default function App() {
   return (
-    <div id="app">
-      <p>Please select an image</p>
-      <p>
-        <input ref={input} data-testid="file-picker" type="file" accept="image/*" />
-        <button onClick={handleClick}>Pick Image</button>
-      </p>
-    </div>
-  );
+        <h1>Ready To Code</h1>
+    );
 }
-
-export default App;
