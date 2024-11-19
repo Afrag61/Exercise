75d26eff1e2f6c495f75d707b99a88018049e0bf
afrag61 <a.frag166@gmail.com>
2024-11-19 04:10:30 +0200

    Ready To Code

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,6 @@
-import React from 'react';
 
-// don't change the Component name "App"
 export default function App() {
-  let [click, setClick] = React.useState()
-    function clickHandler(value){
-      setClick(value)
-        console.log('Clicked!');
-    };
-    
-    return (
-        <div>
-          <h2>You're logged in!</h2>
-          <p>Welcome to your user profile!</p>
-          <button onClick={() => clickHandler(true)}>Click me!</button>
-        </div>
+  return (
+        <h1>Ready To Code</h1>
     );
 }
