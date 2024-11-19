d5538494984e8ba2c726682a0e97be988a98a3af
afrag61 <a.frag166@gmail.com>
2024-11-19 20:11:48 +0200

    Exercise: Accessing DOM Elements with "refs" Solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-
+import React from "react";
 
 function App() {
+  const input = React.useRef()
+
+  function handleClick(){
+    input.current.click()
+  }
+
   return (
     <div id="app">
       <p>Please select an image</p>
       <p>
-        <input data-testid="file-picker" type="file" accept="image/*" />
-        <button>Pick Image</button>
+        <input ref={input} data-testid="file-picker" type="file" accept="image/*" />
+        <button onClick={handleClick}>Pick Image</button>
       </p>
     </div>
   );
