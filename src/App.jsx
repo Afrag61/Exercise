8db4ee3f80b4c346dd8b0dc8c11036aa191e77db
afrag61 <a.frag166@gmail.com>
2024-11-27 10:09:02 +0200

    Coding Exercise 24: Exposing Component APIs

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,17 @@
-import {useState} from "react";
-import { useRef } from "react";
+import Form from './Form';
 
-import Header from "./components/Header";
-import ReactIcon from "./components/ReactIcon";
+// Don't change the name of the 'App' 
+// function and keep it a named export
 
+export default function App() {
 
+  function handleRestart() {}
 
-function App(){
-  const [selectedIndex, setSelectedIndex] = useState([])
-  const [number, setNumber] = useState(0)
-  const [istrue, setistrue] = useState(false)
-  const indexArray = Array.apply(null, Array(number)).map(() => {});
-  const changeNumber = useRef()
-  const warn = 'warn' ;
-
-  function handleChange(){
-    if(+changeNumber.current.value < 5 && +changeNumber.current.value >= 0){
-      setistrue(false)
-      setNumber(+changeNumber.current.value)
-    }else{
-      setistrue(true)
-      setNumber(0)
-    }
-  }
-
-  function handleSelect(index){
-    if(selectedIndex.includes(index)){
-      setSelectedIndex(prev => prev.filter(item => item !== index))
-    }else{
-      setSelectedIndex(prev => [...prev, index])
-    }
+  return (
+    <div id="app">
+      <button onClick={handleRestart}>Restart</button>
+      <Form />
+    </div>
+  );
 }
 
-  return(
-    <>
-      <Header />
-      <div className="inputcontainer">
-        <p>
-          Enter Number of Icons <p className={`limit ${istrue ? warn : undefined}`}>(Max 4):</p>
-        </p>
-        <input className={istrue ? warn : undefined} ref={changeNumber} onChange={handleChange} type="number" />
-      </div>
-      <div className="container">
-        {indexArray.map((item, index) => {
-          return <ReactIcon isSelected={selectedIndex.includes(index)} onClick={() => handleSelect(index)} />
-        })}
-      </div>
-    </>
-  )}
-
-export default App;
---
+++ src/Form.jsx
@@ -0,0 +1,19 @@
+export default function Form() {
+    return (
+      <form>
+        <p>
+          <label>Name</label>
+          <input type="text" />
+        </p>
+  
+        <p>
+          <label>Email</label>
+          <input type="email" />
+        </p>
+        <p id="actions">
+          <button>Save</button>
+        </p>
+      </form>
+    );
+  }
+  
