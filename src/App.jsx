778cd19e373afd53f95a39d3863e36aad9ea67c0
afrag61 <a.frag166@gmail.com>
2024-11-27 10:23:02 +0200

    Ready To Code

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,52 @@
-import React from 'react';
-import Form from './Form';
+import {useState} from "react";
+import { useRef } from "react";
 
-// Don't change the name of the 'App' 
-// function and keep it a named export
+import Header from "./components/Header";
+import ReactIcon from "./components/ReactIcon";
 
-export default function App() {
-  const form = React.useRef()
 
-  function handleRestart() {
-    form.current.clear()
+
+function App(){
+  const [selectedIndex, setSelectedIndex] = useState([])
+  const [number, setNumber] = useState(0)
+  const [istrue, setistrue] = useState(false)
+  const indexArray = Array.apply(null, Array(number)).map(() => {});
+  const changeNumber = useRef()
+  const warn = 'warn' ;
+
+  function handleChange(){
+    if(+changeNumber.current.value < 5 && +changeNumber.current.value >= 0){
+      setistrue(false)
+      setNumber(+changeNumber.current.value)
+    }else{
+      setistrue(true)
+      setNumber(0)
+    }
   }
 
-  return (
-    <div id="app">
-      <button onClick={handleRestart}>Restart</button>
-      <Form ref={form} />
-    </div>
-  );
+  function handleSelect(index){
+    if(selectedIndex.includes(index)){
+      setSelectedIndex(prev => prev.filter(item => item !== index))
+    }else{
+      setSelectedIndex(prev => [...prev, index])
+    }
 }
 
+  return(
+    <>
+      <Header />
+      <div className="inputcontainer">
+        <p>
+          Enter Number of Icons <p className={`limit ${istrue ? warn : undefined}`}>(Max 4):</p>
+        </p>
+        <input className={istrue ? warn : undefined} ref={changeNumber} onChange={handleChange} type="number" />
+      </div>
+      <div className="container">
+        {indexArray.map((item, index) => {
+          return <ReactIcon isSelected={selectedIndex.includes(index)} onClick={() => handleSelect(index)} />
+        })}
+      </div>
+    </>
+  )}
+
+export default App;
