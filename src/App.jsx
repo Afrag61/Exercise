692ec47929256bb2469904c5f22d8c8ecacac4e2
afrag61 <a.frag166@gmail.com>
2024-12-05 13:06:14 +0200

    Coding Exercise 25: Working with Portals ?

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,26 @@
-import {useState} from "react";
-import { useRef } from "react";
+import Toast from './Toast.jsx';
 
-import Header from "./components/Header";
-import ReactIcon from "./components/ReactIcon";
+function App() {
+  function handleEnrol() {
+    // Todo: Show toast
 
-
-
-function App(){
-  const [selectedIndex, setSelectedIndex] = useState([])
-  const [number, setNumber] = useState(0)
-  const [istrue, setistrue] = useState(false)
-  const indexArray = Array.apply(null, Array(number)).map(() => {});
-  const changeNumber = useRef()
-  const warn = 'warn' ;
-
-  function handleChange(){
-    if(+changeNumber.current.value < 5 && +changeNumber.current.value >= 0){
-      setistrue(false)
-      setNumber(+changeNumber.current.value)
-    }else{
-      setistrue(true)
-      setNumber(0)
-    }
+    setTimeout(() => {
+      // Todo: hide toast
+    }, 3000);
   }
 
-  function handleSelect(index){
-    if(selectedIndex.includes(index)){
-      setSelectedIndex(prev => prev.filter(item => item !== index))
-    }else{
-      setSelectedIndex(prev => [...prev, index])
-    }
-}
-
-  return(
-    <>
-      <Header />
-      <div className="inputcontainer">
+  return (
+    <div id="app">
+      {/* Todo: Render <Toast /> component (conditionally) here */}
+      <article>
+        <h2>React Course</h2>
         <p>
-          Enter Number of Icons <p className={`limit ${istrue ? warn : undefined}`}>(Max 4):</p>
+          A course that teaches you React from the ground up and in great depth!
         </p>
-        <input className={istrue ? warn : undefined} ref={changeNumber} onChange={handleChange} type="number" />
-      </div>
-      <div className="container">
-        {indexArray.map((item, index) => {
-          return <ReactIcon isSelected={selectedIndex.includes(index)} onClick={() => handleSelect(index)} />
-        })}
-      </div>
-    </>
-  )}
+        <button onClick={handleEnrol}>Enrol</button>
+      </article>
+    </div>
+  );
+}
 
 export default App;
