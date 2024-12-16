328a046a398e155dcbf57a288b3bc219977d0671
afrag61 <a.frag166@gmail.com>
2024-12-16 15:36:47 +0200

    Coding Exercise 27: Using useReducer() ? Solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,58 @@
-export function counterReducer() {}
+import React from "react";
 
-function App() {
+export const counterReducer = (state, action) => {
+  if(action.type === 'INCREMENT'){
+    return{
+      count: state.count + 1
+    }
+  }
+  if(action.type === 'DECREMENT'){
+    return{
+      count: state.count - 1
+    }
+  }
+  if(action.type === 'RESET'){
+    return{
+      count: state.count = 0
+    }
+  }
+
+  return state
+}
+
+const App = () => {
+  const [counterState, dispatchCounterState] = React.useReducer(counterReducer,{
+    count: 0
+  })
+
+  const handleIncrement = () => {
+    dispatchCounterState({
+      type: 'INCREMENT'
+    })
+  }
+
+  const handleDecrement = () => {
+    dispatchCounterState({
+      type: 'DECREMENT'
+    })
+  }
+
+  const handleReset = () => {
+    dispatchCounterState({
+      type: 'RESET'
+    })
+  }
+
+
   return (
     <div id="app">
       <h1>The (Final?) Counter</h1>
       <p id="actions">
-        <button>Increment</button>
-        <button>Decrement</button>
-        <button>Reset</button>
+        <button onClick={handleIncrement}>Increment</button>
+        <button onClick={handleDecrement}>Decrement</button>
+        <button onClick={handleReset}>Reset</button>
       </p>
-      <p id="counter"></p>
+      <p id="counter">{counterState.count}</p>
     </div>
   );
 }
