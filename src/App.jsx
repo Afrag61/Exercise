49617b360b764d5b7a6a5e6c5e136b9dd220939f
afrag61 <a.frag166@gmail.com>
2024-12-16 15:08:58 +0200

    Coding Exercise 27: Using useReducer() ?

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,17 @@
-import {useState} from "react";
-import { useRef } from "react";
+export function counterReducer() {}
 
-import Header from "./components/Header";
-import ReactIcon from "./components/ReactIcon";
-
-
-
-function App(){
-  const [selectedIndex, setSelectedIndex] = useState([])
-  const [number, setNumber] = useState(0)
-  const [isTrue, setIsTrue] = useState(false)
-  const indexArray = Array.apply(null, Array(number)).map(() => {});
-  const changeNumber = useRef()
-  const warn = 'warn' ;
-
-  function handleChange(){
-    if(+changeNumber.current.value < 5 && +changeNumber.current.value >= 0){
-      setIsTrue(false)
-      setNumber(+changeNumber.current.value)
-    }else{
-      setIsTrue(true)
-      setNumber(0)
-    }
-  }
-
-  function handleSelect(index){
-    if(selectedIndex.includes(index)){
-      setSelectedIndex(prev => prev.filter(item => item !== index))
-    }else{
-      setSelectedIndex(prev => [...prev, index])
-    }
+function App() {
+  return (
+    <div id="app">
+      <h1>The (Final?) Counter</h1>
+      <p id="actions">
+        <button>Increment</button>
+        <button>Decrement</button>
+        <button>Reset</button>
+      </p>
+      <p id="counter"></p>
+    </div>
+  );
 }
-
-  return(
-    <>
-      <Header />
-      <div className="inputcontainer">
-        <p>
-          Enter Number of Icons <p className={`limit ${isTrue ? warn : undefined}`}>(Max 4):</p>
-        </p>
-        <input className={isTrue ? warn : undefined} ref={changeNumber} onChange={handleChange} type="number" />
-      </div>
-      <div className="container">
-        {indexArray.map((item, index) => {
-          return <ReactIcon isSelected={selectedIndex.includes(index)} onClick={() => handleSelect(index)} />
-        })}
-      </div>
-    </>
-  )}
 
 export default App;
